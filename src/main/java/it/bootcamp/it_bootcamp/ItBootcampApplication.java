36225525067ed7d9ba2b2ec91fdbb9a38c304c9e
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2024-01-18 14:43:46 +0300

    Initial commit

---
+++ src/main/java/it/bootcamp/it_bootcamp/ItBootcampApplication.java
@@ -0,0 +1,13 @@
+package it.bootcamp.it_bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItBootcampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/bootcamp/it_bootcamp/ItBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package it.bootcamp.it_bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
